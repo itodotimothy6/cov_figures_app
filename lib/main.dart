62230b9fc4ec29bd62f10bbbb4aea650f7035faf
Timothy Itodo <itodotimothy6@gmail.com>
2020-05-17 16:11:15 -0700

    added theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
+import 'screens/summary_screen.dart';
 
-void main() => runApp(MyApp());
+const kBackgroundColor = Color(0xFF2F2F31);
+const kPrimaryColor = Color(0xFF1B1B32);
+const kDangerColor = Color(0xFFFD434C);
+const kCasualColor = Color(0xFFFEA202);
+const kSafeColor = Color(0xFF7BCA9F);
+const kTextColor = Color(0xFFFAFBF4);
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: CovFiguresApp());
-  }
-}
+void main() => runApp(CovFiguresApp());
 
 class CovFiguresApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+        title: 'Cov Figures',
+        theme: ThemeData(
+          primaryColor: kPrimaryColor,
+          scaffoldBackgroundColor: kBackgroundColor,
+        ),
+        home: SummaryScreen());
   }
 }
