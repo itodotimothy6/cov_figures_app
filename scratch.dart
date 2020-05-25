07e57737767596fbbebfd8a7178e4965f80b297d
Timothy Itodo <itodotimothy6@gmail.com>
2020-05-25 10:12:39 -0700

    modify runner for phone testing

--- scratch.dart
+++ scratch.dart
@@ -1,6 +1,9 @@
-//void main() {
-//  String s;
-//  if (s != null) {
-//    print('yes');
-//  }
-//}
+import 'package:intl/intl.dart';
+
+void main() {
+  var df = DateFormat.yMMMMd('en_US').add_jm();
+
+  var date = DateTime.parse("2020-05-23 18:15");
+
+  print(df.format(date));
+}
